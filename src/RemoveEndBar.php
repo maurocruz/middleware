8b85061f2411abc7a064925a4020dd4b54d4e395
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-08-01 15:49:49 -0300

    Psr style

--- src/RemoveEndBar.php
+++ src/RemoveEndBar.php
@@ -4,20 +4,24 @@ namespace Plinct\Middleware;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Response;
 
 class RemoveEndBar 
 {
     public function __invoke(Request $request, RequestHandler $handler)
     {        
         $uri = $request->getUri();
+        
         $path = $uri->getPath();
         
         if ($path != '/' && substr($path, -1) == '/') {
+                        
             $uri = $uri->withPath(substr($path, 0, -1));
             
             if ($request->getMethod() == 'GET') {
-                $response = new \Slim\Psr7\Response();
+                $response = new Response();
                 return $response->withHeader('Location', (string) $uri)->withStatus(301);
+                
             } else {
                 $request = $request->withUri($uri);
             }
