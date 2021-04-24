a397c02de6b75749483cfe71b6aba1f69ffea0eb
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-24 12:40:37 -0300

    implement Middleware Interface

--- src/RedirectHttps.php
+++ src/RedirectHttps.php
@@ -1,42 +1,34 @@
 <?php
-
 namespace Plinct\Middleware;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-class RedirectHttps
-{
-    public function __invoke(Request $request, RequestHandler $handle) 
-    {   
-        $response = $handle->handle($request);
-        
-        if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.' || $_SERVER['SERVER_PORT'] != 443 && (self::checkForwaredPort(445) === false && self::checkForwaredProto("https") === false)) 
-        {
+class RedirectHttps implements MiddlewareInterface {
+
+    public function process(Request $request, RequestHandler $handler): ResponseInterface {
+        $response = $handler->handle($request);
+        if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.' || $_SERVER['SERVER_PORT'] != 443 && (self::checkForwaredPort() === false && self::checkForwaredProto() === false)) {
             $host = substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.' ? substr($_SERVER['HTTP_HOST'], 4) : $_SERVER['HTTP_HOST'];
-            
-            return $response->withHeader('Location', 'https://' . $host . $_SERVER['REQUEST_URI'])->withStatus(302);            
-            
+            return $response->withHeader('Location', 'https://' . $host . $_SERVER['REQUEST_URI'])->withStatus(302);
         } else {
             return $response;
         }
     }
     
-    private function checkForwaredPort(string $value)
-    {
-        if (array_key_exists('HTTP_X_FORWARDED_PORT', $_SERVER) && $_SERVER['HTTP_X_FORWARDED_PORT'] == $value){
+    private function checkForwaredPort(): bool {
+        if (array_key_exists('HTTP_X_FORWARDED_PORT', $_SERVER) && $_SERVER['HTTP_X_FORWARDED_PORT'] == 445){
             return true;
-            
         } else {
             return false;
         }
     }
     
-    private function checkForwaredProto(string $value)
-    {
-        if (array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == $value){
+    private function checkForwaredProto(): bool {
+        if (array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == "https"){
             return true;
-            
         } else {
             return false;
         }
--- src/RemoveEndBar.php
+++ src/RemoveEndBar.php
@@ -1,32 +1,26 @@
 <?php
-
 namespace Plinct\Middleware;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Psr7\Response;
 
-class RemoveEndBar 
-{
-    public function __invoke(Request $request, RequestHandler $handler)
-    {        
+class RemoveEndBar implements MiddlewareInterface {
+
+    public function process(Request $request, RequestHandler $handler): ResponseInterface {
         $uri = $request->getUri();
-        
         $path = $uri->getPath();
-        
         if ($path != '/' && substr($path, -1) == '/') {
-                        
             $uri = $uri->withPath(substr($path, 0, -1));
-            
             if ($request->getMethod() == 'GET') {
                 $response = new Response();
                 return $response->withHeader('Location', (string) $uri)->withStatus(301);
-                
             } else {
                 $request = $request->withUri($uri);
             }
         }
-
-        return $handler->handle($request);    
+        return $handler->handle($request);
     }
 }
